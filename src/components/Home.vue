355c7fd6b37368ea05a0492b6d6ddf79e585b728
kalir <koushouto@gmail.com>
2021-05-14 10:32:37 +0800

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12> <div v-text="message"></div> </v-flex>
+    <v-flex xs12>
+      <v-btn color="primary" v-on:click="reply">Reply</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "6666",
+    };
+  },
+  methods: {
+    reply() {
+      this.message = "I'm doing great. Thank You!";
+    },
+  },
+};
+</script>
